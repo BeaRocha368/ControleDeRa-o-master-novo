fab1775fa7fcb6c29a367f100111e947cb696cb8
BeaRocha368 <anasrocha9090@gmail.com>
2025-10-06 21:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeRação/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDeRação.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
